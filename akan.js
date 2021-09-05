4cad810ecb5457bfd630c890500f7524297160be
mabunde <solomonsitati39@gmail.com>
2021-09-06 00:28:24 +0300

    Add validate inputs

--- akan.js
+++ akan.js
@@ -1,3 +1,35 @@
 var dayOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
+function validation() {
+    var year = document.getElementById('year').value;
+    var month = document.getElementById('month').value;
+    var day = document.getElementById('date').value;
+    var gender = document.getElementById('gender').value
+
+    var date = new Date(year + "/" + month + "/" + day);
+    var d = date.getDay();
+
+    if((day <= 0) || (day > 31)) {
+        alert('Please enter valid dates!')
+    }
+    else if ((month <= 0) || (month > 12)) {
+        alert('Please enter valid months!')
+    }
+    else if ((year <= 1800) || (year > 2099)) {
+        alert("Check the century entered.")
+    }
+    else if(gender === null){
+        alert("Please choose your gender")
+    }
+
+    if(gender === "Female"){
+        alert("Your Akan name is: " + femaleNames[d] + " and you were born on " + dayOfTheWeek[d])
+    }
+    else if (gender === "Male"){
+        alert("Your Akan name is: " + maleNames[d] + " and you were born on " + dayOfTheWeek[d])
+    }
+
+
+}
